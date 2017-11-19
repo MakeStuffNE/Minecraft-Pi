02d0391f1c0869ad8f16972fa144719719d75967
Make Stuff North East <33810284+MakeStuffNE@users.noreply.github.com>
2017-11-19 18:54:21 +0000

    Create Make_29.py

---
+++ Make_29.py
@@ -0,0 +1,8 @@
+import minecraft.minecraft as minecraft
+mc = minecraft.minecraft.create()
+
+tunnel = [-69,1,104,-66,5,90,103]
+mc.setBlocks(tunnel)
+
+hollow = [-68,2,104,-67,4,90,00]
+mc.setBlocks(hollow)
