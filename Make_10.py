83e0a29c8ac350db3e953e61f24a003673222db3
Make Stuff North East <33810284+MakeStuffNE@users.noreply.github.com>
2017-11-19 17:37:26 +0000

    Create Make_10.py

---
+++ Make_10.py
@@ -0,0 +1,13 @@
+from mcpi.minecraft import Minecraft
+import time
+mc = Minecraft.create()
+
+xPos = 92
+yPos = 1
+zPos = -102
+
+while yPos < 50:
+    blockType = 103
+    mc.setBlock(xPos, yPos, zPos, blockType)
+    yPos = yPos + 1
+    time.sleep (0.2)
