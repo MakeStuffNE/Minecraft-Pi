bffe46177affe09b4aebf7dcb5c727667b7addac
Make Stuff North East <33810284+MakeStuffNE@users.noreply.github.com>
2017-11-19 17:40:31 +0000

    Create Make_11.py

---
+++ Make_11.py
@@ -0,0 +1,14 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+
+xPos = 97
+yPos = 1
+zPos = -102
+
+while yPos < 100:
+    blockType = 103
+    mc.setBlock(xPos, yPos, zPos, blockType)
+    yPos = yPos + 1
+    blockType = 01
+    mc.setBlock(xPos, yPos, zPos, blockType)
+    yPos = yPos + 1
