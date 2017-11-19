cf27010b6ae273bec4eefef7e76cb3564e151829
Make Stuff North East <33810284+MakeStuffNE@users.noreply.github.com>
2017-11-19 18:52:48 +0000

    Create Make_27.py

---
+++ Make_27.py
@@ -0,0 +1,8 @@
+import minecraft.minecraft as minecraft
+mc = minecraft.minecraft.create()
+
+Cube = [-49,1,90,-40,10,99,103]
+mc.setBlocks(cube)
+
+Hollow = [-48,2,91,-41,9,98,00]
+mc.setBlocks(hollow)
